6ab5ca73aca1fe5a35530b1baea631187154c9b6
parglbibl <bibliotekapargolovskaa@gmail.com>
2026-03-19 02:39:17 +0300

    Update common.js

--- common.js
+++ common.js
@@ -100,4 +100,22 @@ document.addEventListener('DOMContentLoaded', function() {
     if (typeof Fancybox !== 'undefined') {
         Fancybox.bind('[data-fancybox]');
     }
+    $('body').append('<div class="upbtn"></div>');
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.upbtn').css({
+            transform: 'scale(1)'
+        });
+        } else {
+        $('.upbtn').css({
+            transform: 'scale(0)'
+        });
+    }
+});
+$('.upbtn').on('click',function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
 });
